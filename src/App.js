3c528ef04265a582455c22d5254858a7e033980c
Matias Martinez <martinez.matias0902@gmail.com>
2021-11-19 11:23:56 -0300

    formatting documents w/prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css'
-import Navbar from './components/NavBar'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/NavBar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
   return (
